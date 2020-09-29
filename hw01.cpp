1a8bfd81b2543e1a9be45aa2d190397ad69b4ab3
kevin22124 <71374999+kevin22124@users.noreply.github.com>
2020-09-30 02:49:14 +0800

    Add files via upload

--- hw01.cpp
+++ hw01.cpp
@@ -1,15 +1,23 @@
- #include <iostream>
- using namespace std;
-int main(int argc, char* argv[]) {
-	 if (argc < 2 || argc > 2) {
-		 cout << "usage:" << endl;
-		 cout << " " << argv[0] << " user" << endl;
-		
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	string user;
+	string vister;
+	cout << "訪客叫什麼名子:";
+	cin >> vister;
+	cout << "主人叫什麼名子";
+	cin >> user;
+	if (argc < 2 || argc > 2)
+	{
+		cout << "usage:" << endl;
+		cout << " " << argv[0] << " user" << endl;
+
 	}
-	 else {
-		 cout << "Hello, " << argv[1] << "." << endl;
-		 cout << "Welcome to the CLI's world." << endl;
-		
+	else {
+		cout << "Hello, " << argv[1] << "." << endl;
+		cout << "Welcome to the "<<user<<" world." << endl;
+
 	}
-	 return 0;
-	}
+	return 0;
+}
