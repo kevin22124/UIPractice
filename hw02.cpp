a6e00c6b3f141476ff77952b15d6e6f3eb2bb3c7
kevin22124 <71374999+kevin22124@users.noreply.github.com>
2020-10-04 00:00:43 +0800

    Add files via upload

--- hw02.cpp
+++ hw02.cpp
@@ -3,25 +3,45 @@
  using namespace std;
  int main(int argc, char* argv[]) 
  {
-	 for (int i = 1; i < argc; i = i++)
+	 if (argc == 1|| argc>3)
 	 {
-		 string arg = argv[i];
-
-		 if (arg == "-?"|| argc !=3)
+		 cout << "錯誤";
+	 }
+	// if (argc > 3)
+	// {
+		// cout << "錯誤";
+	 //}
+	 if (argc < 4)
+	 {
+		 for (int i = 1; i < argc; i = i + 2)
 		 {
-			 cout << "要輸入兩個名子才行";
+			 string arg = argv[i];
+
+			 if (arg == "-?")
+			 {
+				 cout << "要輸入兩個名子才行";
+			 }
+			 else if (argc != 3)
+			 {
+				 cout << "錯誤";
+			 }
+			 else
+			 {
+				 cout << "Hello, " << argv[1] << "." << endl;
+				 cout << "This is " << argv[2] << ", welcome." << endl;
+			 };
 		 }
 	 }
-	 if (argc != 3)
-	 {
-		 cout << "";
-	 }
-	else
-	{
-		 cout << "Hello, " << argv[1] << "." << endl;
-		 cout << "This is " << argv[2] << ", welcome."
-			 << endl;
-	}
+	// if (argc != 3 )
+	// {
+		// cout << "錯誤";
+	 //}
+	//else
+	//{
+		// cout << "Hello, " << argv[1] << "." << endl;
+		 //cout << "This is " << argv[2] << ", welcome."
+			// << endl;
+	//}
 	return 0;
 
 }
